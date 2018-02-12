94c49ca71a16d676e93b4d148a75518d2c8f5678
Gim Beom Uk <marine0436@gmail.com>
2018-02-12 22:29:23 +0900

    open the menu in navigation(my team menu)

--- src/app/com-navi/com-navi.component.ts
+++ src/app/com-navi/com-navi.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-com-navi',
@@ -10,10 +11,29 @@ export class ComNaviComponent implements OnInit {
   fillerNav1 = [`Home`, `Recommend`];
   fillerNav2 = [`My Team`];
   fillerNav3 = [`Help`, `Opinion`, `Setup`, `Logout`];
+  fillerNav4 = [`Team A`, `Team B`, `Team C`];
+  fillerNav5 = [`Chatting`, `Meeting`, `화상회의`];
+  open = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  myTeamClick(a: any) {
+    if ($('#icon').css('transform') === 'matrix(6.12323e-17, 1, -1, 6.12323e-17, 0, 0)') {
+      $('#icon').css('transform', 'rotate(0deg)');
+      $('a[name=group]').slideToggle('fast', function() {
+
+      });
+      this.open = true;
+    } else {
+      $('#icon').css('transform', 'rotate(90deg)');
+      $('a[name=group]').slideToggle('fast', function() {
+
+      });
+      this.open = false;
+    }
   }
 
 }
